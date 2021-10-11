391c7ae4444f58acc876fed899d1478c2836621b
Yevhenii Klimov <24evgeniy03@gmail.com>
2021-10-11 10:25:18 +0000

    Replace Tasks.cs

--- Linq/Tasks.cs
+++ Linq/Tasks.cs
@@ -7,7 +7,6 @@ namespace Linq
 {
     public static class Tasks
     {
-        //The implementation of your tasks should look like this:
         public static string TaskExample(IEnumerable<string> stringList)
         {
             return stringList.Aggregate<string>((x, y) => x + y);
@@ -17,32 +16,27 @@ namespace Linq
 
         public static IEnumerable<string> Task1(char c, IEnumerable<string> stringList)
         {
-            //TODO :Delete line below and write your own solution 
-            throw new NotImplementedException();
+            return stringList.Where(s => s.Length > 1 && s.StartsWith(c) && s.EndsWith(c));
         }
 
         public static IEnumerable<int> Task2(IEnumerable<string> stringList)
         {
-            //TODO :Delete line below and write your own solution 
-            throw new NotImplementedException();
+            return stringList.Select(s => s.Length).OrderBy(s => s).AsEnumerable();
         }
 
         public static IEnumerable<string> Task3(IEnumerable<string> stringList)
         {
-            //TODO :Delete line below and write your own solution 
-            throw new NotImplementedException();
+            return stringList.Select(s => s.First().ToString() + s.Last().ToString());
         }
 
         public static IEnumerable<string> Task4(int k, IEnumerable<string> stringList)
         {
-            //TODO :Delete line below and write your own solution 
-            throw new NotImplementedException();
+            return stringList.Where(s => s.Length == k && char.IsDigit(s.ToArray().Last()) && char.IsDigit(s.ToArray().Last())).OrderBy(s => s);
         }
 
         public static IEnumerable<string> Task5(IEnumerable<int> integerList)
         {
-            //TODO :Delete line below and write your own solution 
-            throw new NotImplementedException();
+            return integerList.Where(i => i % 2 != 0).Select(s => s.ToString()).OrderBy(s => int.Parse(s));
         }
 
         #endregion
@@ -51,26 +45,22 @@ namespace Linq
 
         public static IEnumerable<string> Task6(IEnumerable<int> numbers, IEnumerable<string> stringList)
         {
-            //TODO :Delete line below and write your own solution 
-            throw new NotImplementedException();
+            return numbers.Select(n => stringList.FirstOrDefault(s => s.Length == n && char.IsDigit(s[0]))?? "Not found");
         }
 
         public static IEnumerable<int> Task7(int k, IEnumerable<int> integerList)
         {
-            //TODO :Delete line below and write your own solution 
-            throw new NotImplementedException();
+            return integerList.Where(i => i % 2 == 0).Except(integerList.Skip(k)).Reverse();
         }
         
         public static IEnumerable<int> Task8(int k, int d, IEnumerable<int> integerList)
         {
-            //TODO :Delete line below and write your own solution 
-            throw new NotImplementedException();
+            return integerList.TakeWhile(i => i <= d).Union(integerList.Skip(k)).OrderByDescending(i => i);
         }
 
         public static IEnumerable<string> Task9(IEnumerable<string> stringList)
         {
-            //TODO :Delete line below and write your own solution 
-            throw new NotImplementedException();
+            return stringList.GroupBy(s => s.Substring(0, 1));
         }
 
         public static IEnumerable<string> Task10(IEnumerable<string> stringList)
